d00eb30b5b9a58e1569e0a19c2ab60ab7a288a01
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-16 19:26:24 -0600

    :art: Add custom themes

--- meetly-web/src/presentation/app.tsx
+++ meetly-web/src/presentation/app.tsx
@@ -1,9 +1,15 @@
 export default function MeetlyApp() {
   return (
-    <div className="bg-gradient-to-br from-slate-900 to-slate-700 min-h-screen flex items-center justify-center">
+    <div className="bg-gradient-to-br from-midnight to-tahiti min-h-screen flex items-center justify-center">
       <h1 className="text-4xl text-white font-bold underline">
         Meetly App!!
       </h1>
+
+      <p
+        className="text-primary"
+      >
+        Test
+      </p>
     </div>
   )
 }
