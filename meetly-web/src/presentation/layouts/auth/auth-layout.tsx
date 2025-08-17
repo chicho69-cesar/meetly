e8da446ab5183d964dd9e42733da51b461625e17
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-17 12:53:57 -0600

    :sparkles: Add auth layout and dashboard basic page

--- meetly-web/src/presentation/layouts/auth/auth-layout.tsx
+++ meetly-web/src/presentation/layouts/auth/auth-layout.tsx
@@ -2,8 +2,28 @@ import { Outlet } from "react-router"
 
 export default function AuthLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-primary/30 to-background">
+      <main className="grid grid-cols-1 md:grid-cols-2 bg-white rounded-lg shadow-lg max-w-3xl w-full overflow-hidden">
+        <div className="w-full p-8 md:p-12 flex flex-col items-center justify-center">
+          <img
+            src="/favicon.png"
+            alt="Meetly Logo"
+            className="w-48 h-48 mb-2 drop-shadow-md select-none"
+          />
+  
+          <h1 className="text-4xl font-bold text-primary mb-4 tracking-tight text-center">
+            Bienvenido a Meetly
+          </h1>
+          
+          <p className="text-secondary text-center text-sm font-semibold">
+            Inicia sesión o regístrate para continuar
+          </p>
+        </div>
+          
+        <div className="w-full bg-accent/10">
+          <Outlet />
+        </div>
+      </main>
     </div>
   )
 }
--- meetly-web/src/presentation/pages/dashboard/dashboard-page.tsx
+++ meetly-web/src/presentation/pages/dashboard/dashboard-page.tsx
@@ -1,6 +1,24 @@
+import useAuthStore from "../../hooks/use-auth-store"
 
 export default function DashboardPage() {
+  const { handleLogout } = useAuthStore()
+
   return (
-    <div>DashboardPage</div>
+    <div>
+      <h1 className="text-3xl font-bold text-primary text-center mb-4">
+        Dashboard
+      </h1>
+
+      <p className="text-lg text-secondary mb-4">
+        Bienvenido al panel de control.
+      </p>
+
+      <button
+        onClick={handleLogout}
+        className="px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary/90 transition"
+      >
+        Cerrar sesión
+      </button>
+    </div>
   )
 }
