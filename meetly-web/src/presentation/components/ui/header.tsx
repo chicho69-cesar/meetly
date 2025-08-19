55c3700ba64a2d05677b1ac5c65f94170a8ffa5b
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-19 00:08:12 -0600

    :sparkles: Add open modal from header

--- meetly-web/src/presentation/components/ui/header.tsx
+++ meetly-web/src/presentation/components/ui/header.tsx
@@ -1,7 +1,10 @@
 import { Bell, Search, Settings, User } from "lucide-react"
 import { Link } from "react-router"
+import useUI from "../../hooks/use-ui"
 
 export default function Header() {
+  const { openModal } = useUI()
+
   return (
     <header className="w-full py-4">
       <nav className="w-full max-w-6xl mx-auto flex items-center justify-between">
@@ -29,7 +32,7 @@ export default function Header() {
             autoCorrect="off"
             className="flex-1 px-5 py-2 bg-transparent outline-none text-base text-color placeholder:text-secondary"
           />
-          
+
           <button
             className="px-4 py-2 text-primary flex items-center justify-center rounded-r-full focus:outline-none cursor-pointer font-bold"
             aria-label="Buscar"
@@ -40,15 +43,24 @@ export default function Header() {
         </div>
 
         <div className="flex items-center justify-end space-x-1">
-          <button className="p-2 rounded-full hover:bg-primary/10 transition-colors duration-200 cursor-pointer group">
+          <button
+            className="p-2 rounded-full hover:bg-primary/10 transition-colors duration-200 cursor-pointer group"
+            onClick={() => openModal("bell")}
+          >
             <Bell className="w-6 h-6 text-secondary group-hover:rotate-12 group-hover:text-primary transition" />
           </button>
 
-          <button className="p-2 rounded-full hover:bg-primary/10 transition-colors duration-200 cursor-pointer group">
+          <button
+            className="p-2 rounded-full hover:bg-primary/10 transition-colors duration-200 cursor-pointer group"
+            onClick={() => openModal("settings")}
+          >
             <Settings className="w-6 h-6 text-secondary group-hover:rotate-12 group-hover:text-primary transition" />
           </button>
 
-          <button className="p-2 rounded-full hover:bg-primary/10 transition-colors duration-200 cursor-pointer group">
+          <button
+            className="p-2 rounded-full hover:bg-primary/10 transition-colors duration-200 cursor-pointer group"
+            onClick={() => openModal("user")}
+          >
             <User className="w-6 h-6 text-secondary group-hover:rotate-12 group-hover:text-primary transition" />
           </button>
         </div>
--- meetly-web/src/presentation/layouts/dashboard/dashboard-layout.tsx
+++ meetly-web/src/presentation/layouts/dashboard/dashboard-layout.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router"
 import Header from "../../components/ui/header"
+import Modal from "../../components/ui/modal"
+import useUI from "../../hooks/use-ui"
 
 export default function DashboardLayout() {
+  const { modal, closeModal } = useUI()
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-primary/30 to-background">
       <Header />
       <Outlet />
+
+      <Modal open={!!modal} onClose={closeModal}>
+        {modal === "bell" && <div>Notificaciones aquí</div>}
+        {modal === "settings" && <div>Opciones de configuración aquí</div>}
+        {modal === "user" && <div>Opciones de usuario aquí</div>}
+      </Modal>
     </div>
   )
 }
