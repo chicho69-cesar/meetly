905cff0c610cf870f7531b685852afd3ba774546
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-19 09:32:08 -0600

    :sparkles: Add calendar view

--- meetly-web/src/presentation/pages/dashboard/dashboard-page.tsx
+++ meetly-web/src/presentation/pages/dashboard/dashboard-page.tsx
@@ -1,8 +1,55 @@
-import Calendar from "../../components/calendar/calendar/calendar"
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import "./dashboard-page.styles.css"
+
+import { useState } from "react"
+import { Calendar, Views, type SlotInfo, type View } from "react-big-calendar"
+
+import { localizer, MESSAGES_ES } from "../../../config/adapters/calendar"
+import CalendarEvent from "../../components/calendar/calendar-event/calendar-event"
 import useAuthStore from "../../hooks/use-auth-store"
 
 export default function DashboardPage() {
   const { handleLogout } = useAuthStore()
+
+  const [events, setEvents] = useState([])
+  const [lastView, setLastView] = useState<View>(Views.WEEK)
+
+  const eventStyleGetter = (event: unknown, start: Date, end: Date, isSelected: boolean) => {
+    console.log({ event, start, end, isSelected })
+    // const isMyEvent = (user.uid === event.user._id) || (user.uid === event.user.uid)
+
+    const style = {
+      // backgroundColor: isMyEvent ? "#347CF7" : "#465660",
+      backgroundColor: "#347CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    }
+
+    return {
+      style,
+    }
+  }
+
+  const handleDoubleClick = () => {
+    console.log("Double clicked on event")
+    // openDateModal()
+  }
+
+  const handleSelect = (event: unknown) => {
+    console.log("Selected event:", event)
+    // openDateModal(event)
+  }
+
+  const handleViewChange = (view: View) => {
+    console.log("View changed to:", view)
+    setLastView(view)
+    // localStorage.setItem("lastView", view)
+  }
+
+  const handleSelectSlot = (slotInfo: SlotInfo) => {
+    console.log("Selected slot:", slotInfo)
+  }
 
   return (
     <main className="w-full mx-auto max-w-6xl my-12">
@@ -21,7 +68,26 @@ export default function DashboardPage() {
         Cerrar sesión
       </button>
 
-      <Calendar />
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        defaultView={lastView}
+        view={lastView}
+        startAccessor="start"
+        endAccessor="end"
+        selectable
+        style={{ height: "calc( 100vh - 80px )", marginTop: "20px" }}
+        messages={MESSAGES_ES}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+        onSelectSlot={handleSelectSlot}
+        onDoubleClickEvent={handleDoubleClick}
+        onSelectEvent={handleSelect}
+        onView={handleViewChange}
+      />
     </main>
   )
 }
