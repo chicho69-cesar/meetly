12b6978806694676111d98b20477d0cf8de76722
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-23 21:59:42 -0600

    :sparkles: Add tasks and storage constants

--- meetly-web/src/config/constants/storage.constant.ts
+++ meetly-web/src/config/constants/storage.constant.ts
@@ -1,1 +1,2 @@
 export const THEME_STORAGE_KEY = "__meetly_theme__"
+export const TASK_VIEW_STORAGE_KEY = "__meetly_task_view__"
---
+++ meetly-web/src/config/constants/tasks.constant.ts
@@ -0,0 +1,2 @@
+export const PRIORITY_OPTIONS = ["Alta", "Media", "Baja"]
+export const STATUS_OPTIONS = ["Pendiente", "En progreso", "Completada"]
